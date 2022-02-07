678cf88ca474503db11813b16da7b92aa7bae7ff
halitbatur <halitfuatbatur@gmail.com>
2022-02-07 10:17:09 +0300

    clear main

--- src/move.ts
+++ src/move.ts
@@ -1,9 +1,1 @@
-import { Folder, FilePathChangeIndexMap } from './types';
-import { findNeededIndexes, throwIfIndexIsInvalid, moveFileWithinList } from './lib';
-
-export default function move(list: Folder[], source: string, destination: string): Folder[] {
-  const indexes: FilePathChangeIndexMap = findNeededIndexes(list, source, destination);
-
-  const validatedIndexes = throwIfIndexIsInvalid(indexes);
-  return moveFileWithinList(list, validatedIndexes);
-}
+export default function move(list: Folder[], source: string, destination: string): Folder[] {}
